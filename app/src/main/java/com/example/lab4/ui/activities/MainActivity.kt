bd622688c553cb246baca7b7a2df7c506d6c6b18
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-09-28 20:11:19 +0700

    feat: add toast notifications on btns

--- app/src/main/java/com/example/lab4/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/lab4/ui/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lab4.ui.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.lab4.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +22,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { _: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+        falseButton.setOnClickListener { _: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
