1ff96a0669ec837807ab22f302c0d3196ef264a5
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-09-29 18:34:35 +0700

    fix: empty class body

--- app/src/main/java/com/example/lab4/models/Question.kt
+++ app/src/main/java/com/example/lab4/models/Question.kt
@@ -2,6 +2,4 @@ package com.example.lab4.models
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean) {
-
-}
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
